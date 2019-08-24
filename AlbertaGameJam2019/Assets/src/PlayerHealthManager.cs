d26968e8c185cc968fd84ad15b6a1e5a1b2a5907
Marcotte173 <travis.marcotte173@gmail.com>
2019-08-24 15:18:18 -0600

    World Ui

---
+++ AlbertaGameJam2019/Assets/src/PlayerHealthManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    public int totalHealth;
+    public int currentHealth;
+    public bool isPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = totalHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UIManager.instance.maxHealth = totalHealth;
+        UIManager.instance.currentHealth = currentHealth;
+        
+    }
+
+    public void takeDamage(int damage)
+    {
+        currentHealth += -damage;
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
