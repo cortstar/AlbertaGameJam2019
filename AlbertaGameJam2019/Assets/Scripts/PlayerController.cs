c74d4a976b9b172db65832c707ecfdc44941e57d
Marcotte173 <travis.marcotte173@gmail.com>
2019-08-24 02:16:36 -0600

    Character movement tied to Nav Mesh

--- AlbertaGameJam2019/Assets/Scripts/PlayerController.cs
+++ AlbertaGameJam2019/Assets/Scripts/PlayerController.cs
@@ -1,41 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
-    CharacterController characterController;
-    public float speed = 6.0f;
-    public static PlayerController instance;
-    private Vector3 moveDirection = Vector3.zero;
+    public Transform target;
 
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            if (instance != this)
-            {
-                Destroy(gameObject);
-            }
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Move Character
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-        moveDirection *= speed;
-        characterController.Move(moveDirection * Time.deltaTime);
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.position;
 
         //Rotate Character
-        Vector3 PlayerDirection = Vector3.right  * Input.GetAxis("RHorizontal") + Vector3.forward * -Input.GetAxis("RVertical");
-        if(PlayerDirection.sqrMagnitude > 0.0f)
+        Vector3 PlayerDirection = Vector3.right * Input.GetAxis("RHorizontal") + Vector3.forward * -Input.GetAxis("RVertical");
+        if (PlayerDirection.sqrMagnitude > 0.0f)
         {
             transform.LookAt(transform.position + PlayerDirection);
         }
