455d3dc55d0bc3d7b9661d06e34dc2d2c68d38a7
cortstar <treepunch2@gmail.com>
2019-08-24 14:54:04 -0600

    no message

---
+++ AlbertaGameJam2019/Assets/LockZ.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockZ : MonoBehaviour
+{
+    private Transform trans;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        trans = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        trans.rotation = Quaternion.Euler(90f, 0f, 0f);
+    }
+}
