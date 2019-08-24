0a620218baf6106e0dad7c74ce59568030c460c6
cortstar <treepunch2@gmail.com>
2019-08-23 22:23:44 -0600

    skills v2

---
+++ AlbertaGameJam2019/Assets/Skill/PlayerSkill.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public abstract class PlayerSkill : MonoBehaviour
+{
+    public abstract Timer Cooldown { get; set; }
+
+    /// <summary>
+    /// Returns true if the skill is off cooldown
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns></returns>
+    public bool Use(GameObject user)
+    {
+        if (!Cooldown.IsComplete)
+        {
+            return false;
+        }
+
+        Cooldown.Reset();
+        Cooldown.Unpause();
+        Behave(user);
+            
+        return true;
+
+    }
+    protected abstract void Behave(GameObject User);
+
+    void Update()
+    {
+        Cooldown.Update();
+    }
+}
---
+++ AlbertaGameJam2019/Assets/Skill/SwordSkill.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordSkill : PlayerSkill
+{
+    public override Timer Cooldown { get; set; }
+    protected override void Behave(GameObject User)
+    {
+        throw new System.NotImplementedException();
+    }
+}
