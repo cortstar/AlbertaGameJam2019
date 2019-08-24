a890aba4a6f39346316868e681e64957d76c3c21
cortstar <treepunch2@gmail.com>
2019-08-24 15:18:10 -0600

    no message

---
+++ AlbertaGameJam2019/Assets/src/Missive/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Missive_CSharp;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class PlayerController : MonoBehaviour, IMissiveListener<SwordInputEvent>, IMissiveListener<GunInputEvent>
+{
+
+    void Start()
+    {
+        MissiveAggregator.instance.Register<SwordInputEvent>(this);
+        MissiveAggregator.instance.Register<GunInputEvent>(this);
+    }
+
+    public void HandleMissive(SwordInputEvent missive)
+    {
+        gameObject.GetComponentSafely<SwordSkill>().Use(gameObject);
+    }
+
+    public void HandleMissive(GunInputEvent missive)
+    {
+        gameObject.GetComponentSafely<PistolSkill>().Use(gameObject);
+    }
+}
