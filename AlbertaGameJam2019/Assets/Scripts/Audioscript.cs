c7e121dc47794269b1eb362f9693d4c494887773
Marcotte173 <travis.marcotte173@gmail.com>
2019-08-24 18:44:14 -0600

    Awesome opening screen

---
+++ AlbertaGameJam2019/Assets/Scripts/Audioscript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audioscript : MonoBehaviour
+{
+
+    public AudioClip Engine;
+
+    public AudioSource source;
+
+    private bool playing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source.clip = Engine;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playing)
+        {
+            source.Play();
+            playing = true;
+        }
+
+    }
+}
