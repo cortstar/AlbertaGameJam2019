c5c7d195099aa43e932d5128a393425b91a8da97
cortstar <treepunch2@gmail.com>
2019-08-24 18:44:37 -0600

    animator

---
+++ AlbertaGameJam2019/Assets/PlayerAnimator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator animator;
+    
+    private 
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponentSafely<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
